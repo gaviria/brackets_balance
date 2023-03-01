efdbc81b600902dc34d193f519f3fd567a8c0063
Francisco <gaviria.reyes@gmail.com>
2023-03-01 09:50:57 -0500

    adding comment and new balance function in order elements

--- src/BracketsBalance.php
+++ src/BracketsBalance.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace Fgaviria;
+
+/**
+ * BracketsBalance
+ */
 class BracketsBalance
 {
     const OPEN_ELEMENTS = ["{", "[", "("];
@@ -11,18 +16,79 @@ class BracketsBalance
     public array $balance = ["(" => 0, "{" => 0, "[" => 0];
 
 
+    /**
+     * push
+     * Push a element
+     * @param  string $item
+     * @return void
+     */
     public function push(string $item)
     {
         array_push($this->items, $item);
-        $this->totalElements += $this->totalElements;
+        $this->totalElements++;
     }
 
-    public function size()
+    /**
+     * size
+     * return total number of elements
+     * @return int
+     */
+    public function size(): int
     {
         return $this->totalElements;
     }
 
-    public function doBracketsBalance(string $strings)
+    /**
+     * pop
+     * Pop a element
+     * @return void
+     */
+    public function pop(): void
+    {
+        if ($this->totalElements > 0) {
+            $this->totalElements--;
+            array_pop($this->items);
+        }
+    }
+
+    /**
+     * doBracketsBalancePlus
+     * Verify balance in order of elements
+     * @param string $strings
+     * @return bool
+     */
+    public function doBracketsBalancePlus(string $strings): bool
+    {
+        $arrayStrings = str_split($strings);
+
+        foreach ($arrayStrings as $string) {
+            if (in_array($string, self::OPEN_ELEMENTS)) {
+                $this->push($string);
+            }
+
+            if (in_array($string, self::CLOSE_ELEMENTS)) {
+                if ($this->size() === 0) {
+                    return false;
+                }
+
+                if ($this->items[$this->size() - 1] === self::ELEMENTS[$string]) {
+                    $this->pop($string);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return $this->size() === 0 ? true : false;
+    }
+
+    /**
+     * doBracketsBalance
+     * Verify balance of elements
+     * @param  string $strings
+     * @return bool
+     */
+    public function doBracketsBalance(string $strings): bool
     {
         $arrayStrings = str_split($strings);
 
@@ -40,6 +106,10 @@ class BracketsBalance
             }
         }
 
-        echo array_sum($this->balance) === 0 ? "true" : "false";
+        return array_sum($this->balance) === 0 ? true : false;
     }
 }
+
+$bracketsBalance = new BracketsBalance();
+echo "doBracketsBalance => " . ($bracketsBalance->doBracketsBalance("()[(])") ? "True" : "False"); //true
+echo "doBracketsBalance => " . ($bracketsBalance->doBracketsBalancePlus("()[(])") ? "True" : "False"); //false
